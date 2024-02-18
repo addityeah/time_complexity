1ccd14b2f7f440c0d97e82829096a5094a204ec4
Aditya-550 <primeking2004@gmail.com>
2024-02-18 20:02:16 +0530

    Modified bestfit.py

--- bestfit.py
+++ bestfit.py
@@ -1,11 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit
 
-x = np.arrange(0, 10, 0.1)
-y = np.array([0.1, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1])
+def curve_func(n, a, option):
+    if (option == 1):
+        return a * n
+    
+    elif (option == 2):
+        return a * n ** 2
+    
+    elif (option == 3):
+        return a * np.log(n)
+    
+    elif (option == 4):
+        return a * n * np.log(n)
+    
 
-plt.scatter(x, y)
-plt.show()
+def main():
+    option = input("Enter the type of curve you want to fit: \n 1. Linear \n 2. Quadratic \n 3. Logarithmic \n 4. nlogn \n")
+    
+    curve = curve_func(option)
+    
+    x = np.array([0, 1, 2, 3, 4, 5])
+    y = np.array([0, 0.8, 0.9, 0.1, -0.8, -1.0])
 
-fit = np.polyfit(x, y, 1)
+    # Fit the curve to the data using curve_fit
+    params, covariance = curve_fit(curve, x, y)
+
+    # Extract the fitted parameters
+    a_fit = params
+
+    # Generate y values using the fitted parameters
+    y_fit = curve_func(x, a_fit)
