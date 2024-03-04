b0972b27705f1b1745f242a1d7c4c4e38e6c39f1
Aditya-550 <primeking2004@gmail.com>
2024-03-04 19:21:05 +0530

    Update bestfit.py

--- bestfit.py
+++ bestfit.py
@@ -18,8 +18,8 @@ def curve4(x, a, b, c):
     return a * np.log2(x) * x + b * x + c
     
 def main():
-    x = np.array([0, 1, 2, 3, 4, 5])
-    y = np.array([0, 0.8, 0.9, 0.1, -0.8, -1.0])
+    x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+    y = np.array([0, 0.8, 0.9, 0.1, -0.8, -1.0, -0.5, 0.4, 0.9, 1.0, 0.5, -0.4, -0.9, -1.0, -0.5, 0.4, 0.9, 1.0, 0.5, -0.4, -0.9])
 
     # Fit the curve to the data using curve_fit
     fitp0, fite0 = curve_fit(curve0, x, y)
@@ -28,4 +28,26 @@ def main():
     fitp3, fite3 = curve_fit(curve3, x, y)
     fitp4, fite4 = curve_fit(curve4, x, y)
     
-    print(fite0, fite1, fite2, fite3, fite4)
+    y1 = curve0(x, *fitp0)
+    y1 = curve0(x, *fitp0)
+    y2 = curve1(x, *fitp1)
+    y3 = curve2(x, *fitp2)
+    y4 = curve3(x, *fitp3)
+    y5 = curve4(x, *fitp4)
+
+    plt.plot(x, y, 'o', label='data')
+    plt.plot(x, y1, label='fit0')
+    plt.plot(x, y2, label='fit1')
+    plt.plot(x, y3, label='fit2')
+    plt.plot(x, y4, label='fit3')
+    plt.plot(x, y5, label='fit4')
+    plt.legend()
+    plt.show()
+
+    print(fite0)
+    print(fite1)
+    print(fite2)
+    print(fite3)
+    print(fite4)
+
+    return 0
