f5af24ebdeff044d0f297c894d00969af2340379
Aditya-550 <primeking2004@gmail.com>
2024-04-22 11:34:37 +0530

    tryball to be used

--- new_build/tryball.py
+++ new_build/tryball.py
@@ -1,23 +1,18 @@
 import subprocess
 import time
 
-# Path to the C++ program
-cpp_program = 'program_path'
+cpp_program = '../L5_template/Q1_Template.cpp'
 
-# Path to the input text file
 input_file = 'testcases.txt'
 
-# Read the input from the text file
 with open(input_file, 'r') as file:
     input_data = file.read().strip()
 
-# Run the C++ program and measure the time taken
 start_time = time.time()
-subprocess.run(['g++', cpp_program])  # Compile the C++ program
-subprocess.run(['./a.out', input_data])  # Run the C++ program with the input data
+subprocess.run(['g++', cpp_program]) 
+subprocess.run(['./a.out', input_data]) 
 end_time = time.time()
 
-# Calculate the time taken in seconds
 execution_time = end_time - start_time
 
 print(execution_time)
